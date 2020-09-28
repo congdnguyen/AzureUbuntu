43ff5e0cca015be7e8d0f63c3377b608f86364cb
congdnguyen <34898278+congdnguyen@users.noreply.github.com>
2020-09-28 12:37:33 -0700

    Update dhcp-domain-join.sh

--- dhcp-domain-join.sh
+++ dhcp-domain-join.sh
@@ -1,57 +1,52 @@
 #!/usr/bin/env bash
 
-#get all input from user
-#host input
-read -p "What is host's name: " hname
-read -p "What is host's IP: " mip
-
-#domain input
-read -p "What is domain name, must be in CAP e.g. CWQI.NET: " vdom
-read -p "What is domain admin name: " dname
-
 #set time-zone Pacific time
 #sudo timedatectl set-timezone America/Los_Angeles
 
 #update and install realmd
 sudo apt-get update
-sudo apt-get install krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli
+sudo apt-get install -y krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli
 
 #read host input
-#moved to begining
+read -p "What is host's name: " hname
+read -p "What is host's IP: " mip
+read -p "What is domain's name: " dname
 
 #set hosts file /etc/hosts
 gip=`ip a | grep $mip | awk '{print $2}' | awk -F/ '{print $1}'`
-sudo echo "$gip $hname.domain.com name $hname" >> /etc/hosts
+echo "$gip $hname.$dname $hname" >> /etc/hosts
+#sed -i '/127.0.0.1/a$gip $hname.$dname $hname' /etc/hosts
 
 #add domain to ntp.conf
-sudo echo -e "\nserver cwqi.net" >> etc/ntp.conf
+echo -e "\nserver $dname" >> /etc/ntp.conf
 
-sudo systemctl stop ntp
-sudo ntpdate aaddscontoso.com
-sudo systemctl start ntp
+systemctl stop ntp
+ntpdate $dname
+systemctl start ntp
 
 #read domain input
-#moved to begining
+read -p "Please provide domain name again, must be in CAP e.g. COMPANY.NET : " dname
+read -p "What is domain Administrator's name: " adname
 
 #join to domain
-sudo realm join --verbose $vdom -U '$dname@$vdom' --install=/
+realm join --verbose $dname -U "$adname@$dname" --install=/
 
 #set rnds
-sudo sed -i '/default_realm/a rnds=false' testfile /etc/krb5.conf
+sed -i '/default_realm/a rnds=false' /etc/krb5.conf
 
 #use just username without long FQDN
-sudo sed -i 's/use_fully_qualified_names = True/#use_fully_qualified_names = True/' /etc/sssd/sssd.conf
-sudo sed -i 's/fallback_homedir = \/home\/%u@%d/fallback_homedir = \/home\/%u/' /etc/sssd/sssd.conf
+sed -i 's/use_fully_qualified_names = True/#use_fully_qualified_names = True/' /etc/sssd/sssd.conf
+sed -i 's/fallback_homedir = \/home\/%u@%d/fallback_homedir = \/home\/%u/' /etc/sssd/sssd.conf
 
 #allow domain users login xrdp
-sudo sed -i 's/access_provider = ad/access_provider = simple/' /etc/sssd/sssd.conf
+sed -i 's/access_provider = ad/access_provider = simple/' /etc/sssd/sssd.conf
 
-sudo systemctl restart sssd
+systemctl restart sssd
 
 #allow ssh login using password
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
-sudo systemctl restart ssh
+systemctl restart ssh
 
 #append new line, create user's folder
-sudo sed -i '/pam_sss.so/a session optional pam_sss.so' testfile /etc/pam.d/common-session
+sed -i '/pam_sss.so/a session optional pam_sss.so' /etc/pam.d/common-session
